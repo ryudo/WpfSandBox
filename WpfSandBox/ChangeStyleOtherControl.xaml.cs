a835f0684e7601c5dbc07232a275852e94c4f8c6
Ryudo NAKAGAWA <nkgw.ry@gmail.com>
2012-06-25 21:43:13 +0900

    コントロール名を変えた

---
+++ WpfSandBox/ChangeStyleOtherControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfSandBox
+{
+    /// <summary>
+    /// ChangeStyleOtherControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ChangeStyleOtherControl
+    {
+        public static readonly DependencyProperty IsButtonMouseOverProperty = DependencyProperty.Register("IsButtonMouseOver",
+    typeof(bool), typeof(ChangeStyleOtherControl), new PropertyMetadata(false));
+
+        public bool IsButtonMouseOver
+        {
+            get { return (bool)GetValue(IsButtonMouseOverProperty); }
+            set { SetValue(IsButtonMouseOverProperty, value); }
+        }
+
+        public ChangeStyleOtherControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1MouseEnter(object sender, MouseEventArgs e)
+        {
+            IsButtonMouseOver = button1.IsMouseOver;
+        }
+
+        private void Button1MouseLeave(object sender, MouseEventArgs e)
+        {
+            IsButtonMouseOver = button1.IsMouseOver;
+        }
+    }
+}
