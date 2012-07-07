dac86035c08158bcfa10530a266a464954008011
Ryudo NAKAGAWA <nkgw.ry@gmail.com>
2012-07-08 07:58:47 +0900

    長丸にbindingを設定した

--- WpfSandBox/CornarRadiusConverter.cs
+++ WpfSandBox/CornarRadiusConverter.cs
@@ -1,8 +1,20 @@
 ﻿namespace WpfSandBox
 {
+    using System;
+    using System.Globalization;
     using System.Windows.Data;
 
     class CornarRadiusConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var doubleValue = (double)value;
+            return doubleValue / 2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- WpfSandBox/EllipseControl.xaml.cs
+++ WpfSandBox/EllipseControl.xaml.cs
@@ -19,9 +19,26 @@ namespace WpfSandBox
     /// </summary>
     public partial class EllipseControl : UserControl
     {
+        /// <summary>
+        /// The aspect property.
+        /// </summary>
+        public static DependencyProperty AspectProperty = DependencyProperty.Register(
+            "Aspect", typeof(double), typeof(EllipseControl), new PropertyMetadata(2.0));
+
         public EllipseControl()
         {
             InitializeComponent();
+            this.SizeChanged += (sender, args) =>
+                { double aspect =  (this.outerGrid.ActualWidth / this.outerGrid.ActualHeight) /2;
+                    this.SetValue(AspectProperty, aspect);};
+        }
+
+        public double Aspect
+        {
+            get
+            {
+                return (double)this.GetValue(AspectProperty);
+            }
         }
     }
 }
