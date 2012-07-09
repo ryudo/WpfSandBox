43c7f90cf7a783b220479d52fd0a5e1ac809f16d
Ryudo NAKAGAWA <nkgw.ry@gmail.com>
2012-07-09 09:01:04 +0900

    expandを変えてみた

--- WpfSandBox/Expand.xaml.cs
+++ WpfSandBox/Expand.xaml.cs
@@ -28,13 +28,6 @@ namespace WpfSandBox
             InitializeComponent();
             this.Loaded += (d, e) =>
                 {
-                    Line l1 = new Line();
-                    l1.Stroke = new SolidColorBrush(Colors.Blue);
-                    l1.X1 = 0;
-                    l1.Y1 = 0;
-                    l1.X2 = rightTop.X;
-                    l1.Y2 = rightTop.Y;
-                    this.outerGrid.Children.Add(l1);
                 };
             this.SizeChanged += (sender, args) =>
                 { 
