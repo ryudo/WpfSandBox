765b8cbc3c81d338c39916d0cc476ffae5b9a621
Ryudo NAKAGAWA <nkgw.ry@gmail.com>
2012-07-11 09:25:20 +0900

    NLogをいれた

--- WpfSandBox/MainWindow.xaml.cs
+++ WpfSandBox/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WpfSandBox
 {
+	using NLog;
 	/// <summary>
 	/// MainWindow.xaml の相互作用ロジック
 	/// </summary>
@@ -23,6 +12,12 @@ namespace WpfSandBox
 		{
 			InitializeComponent();
 			Resources["res1"] = Properties.Settings.Default.TextColor;
+		}
+
+		private void button1_Click(object sender, RoutedEventArgs e)
+		{
+			Logger logger = LogManager.GetLogger("");
+			logger.Info("tesuto");
 		}
 	}
 }
