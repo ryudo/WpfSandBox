bea23abe68174835ee3b94d104aba5e518b40f48
Ryudo NAKAGAWA <nkgw.ry@gmail.com>
2012-07-11 13:42:15 +0900

    押したら変わるアイコン

---
+++ WpfSandBox/IconAttach.cs
@@ -0,0 +1,100 @@
+﻿namespace WpfSandBox
+{
+	using System.ComponentModel;
+	using System.Windows.Media;
+
+	class IconAttach : INotifyPropertyChanged
+	{
+		private ImageSource _on;
+		private ImageSource _off;
+		private ImageSource _icon;
+
+		private bool _isOn;
+
+
+		public ImageSource OnIcon
+		{
+			get
+			{
+				return _off;
+			}
+			set
+			{
+				if (value != _on)
+				{
+					_on = value;
+					this.RaisePropertyChanged("OnIcon");
+				}
+			}
+		}
+
+		public ImageSource OffIcon
+		{
+			get
+			{
+				return _on;
+			}
+			set
+			{
+				if (value != _off)
+				{
+					_off = value;
+					this.RaisePropertyChanged("OffIcon");
+				}
+			}
+		}
+
+		public ImageSource Icon
+		{
+			get
+			{
+				if(this.IsOn)
+				{
+					return _on;
+				}
+				else
+				{
+					return _off;
+				}
+			}
+			set
+			{
+				_icon = value;
+				this.RaisePropertyChanged("Icon");
+			}
+		}
+
+		public bool IsOn
+		{
+			get
+			{
+				return _isOn;
+			}
+			set
+			{
+				if (value != _isOn)
+				{
+					_isOn = value;
+					if (_isOn)
+					{
+						this.Icon = this.OnIcon;
+					}
+					else
+					{
+						this.Icon = this.OffIcon;
+					}
+					this.RaisePropertyChanged("IsOn");
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void RaisePropertyChanged(string propertyName)
+		{
+			var d = PropertyChanged;
+			if (d != null)
+				d(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
---
+++ WpfSandBox/IconAttachButton.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSandBox
+{
+	/// <summary>
+	/// アイコンが入れれるボタン
+	/// </summary>
+	public class IconAttachButton : Button
+	{
+		private IconAttach _iconAttach = new IconAttach();
+
+		static IconAttachButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(IconAttachButton), new FrameworkPropertyMetadata(typeof(IconAttachButton)));
+		}
+
+		public IconAttachButton()
+		{
+			this.DataContext = this._iconAttach;
+		}
+
+		public ImageSource OnIcon
+		{
+			get
+			{
+				return _iconAttach.OnIcon;
+			}
+
+			set
+			{
+				_iconAttach.OnIcon = value;
+			}
+		}
+
+		public ImageSource OffIcon
+		{
+			get
+			{
+				return _iconAttach.OffIcon;
+			}
+
+			set
+			{
+				_iconAttach.OffIcon = value;
+			}
+		}
+
+		protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+		{
+			base.OnPropertyChanged(e);
+			if (e.Property == Button.IsPressedProperty)
+			{
+				_iconAttach.IsOn = (bool)e.NewValue;
+			}
+		}
+	}
+}
